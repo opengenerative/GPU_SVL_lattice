ea03a2eab6f254796958a7fced57a22165a83da5
Tom <opengenerative@gmail.com>
2024-06-20 08:35:07 +0530

    Update File_output.h
    avoiding spaces

--- File_output.h
+++ File_output.h
@@ -2,18 +2,13 @@
 #ifndef _FILE_OUTPUT_H_
 #define _FILE_OUTPUT_H_
 
-
-
-
 class File_output
 {
     public:
-    File_output();
-    ~File_output();
 
     void file_write(float4 *d_pos,uint totalVerts, const char* filename);
 
 };
 
 
-#endif // // _FILE_OUTPUT_H_
+#endif //_FILE_OUTPUT_H_
