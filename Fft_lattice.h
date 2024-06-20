33710eaaaf8a4a84ac7877880d75cad7fe1790b1
Tom <opengenerative@gmail.com>
2024-06-20 08:33:21 +0530

    Update Fft_lattice.h
    avoiding spaces

--- Fft_lattice.h
+++ Fft_lattice.h
@@ -1,7 +1,5 @@
 #pragma once
 
-
-
 #ifndef __FFT_LATTICE_H__
 #define __FFT_LATTICE_H__
 
@@ -10,8 +8,6 @@ class Fft_lattice
 {
     public:
 
-    Fft_lattice();
-    ~Fft_lattice();
     void create_lattice(float *d_latticevol, uint NX, uint NY, uint NZ, uint size);
     void fft_func(float2 *fft_data);
     void ifft_func(float2 *fft_data);
